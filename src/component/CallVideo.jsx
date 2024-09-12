7c2c90a8ef8ed20341f9e3d3e077b65c60c9ff7f
zhoang-2 <huyhoangzzzzb@gmail.com>
2024-09-12 21:21:50 +0700

    layout

---
+++ src/component/CallVideo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CallVideo = () => {
+  return (
+    <div>CallVideo</div>
+  )
+}
+
+export default CallVideo
---
+++ src/pages/individual.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const individual = () => {
+  return (
+    <div>individual</div>
+  )
+}
+
+export default individual
